858b3e38616e08133c5254d81d4860ded9a9bdcd
AkopyaSha <akopyasha@bk.ru>
2020-06-06 19:19:59 +0300

    Initial commit

---
+++ src/test/java/ru/arl/SpringTest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arl.SpringTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
